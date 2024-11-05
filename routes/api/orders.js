a4ec6a6d4d870cea7d3a27d11929234da0712d19
Aibo Li <aibli@microsoft.com>
2024-11-04 18:09:26 -0800

    update order request body

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -9,7 +9,7 @@ const salt = bcrypt.genSaltSync(saltRounds);
 const authentication = require("../../middlewares/authentication");
 
 /* GET users listing. */
-router.post("/", authentication, async function(req, res, next) {
+router.post("/", authentication, async function (req, res, next) {
   const accessToken = req.cookies.access_token || req.headers.authorization;
   const authData = jwt.verify(accessToken, "yolofootball");
   const userName = authData.data;
@@ -19,7 +19,9 @@ router.post("/", authentication, async function(req, res, next) {
     bet_result: postbody.bet_result,
     odd_mount: postbody.odd_mount,
     fixture_state: postbody.fixture_state,
-    user_name: userName
+    odd_rate: postbody.odd_rate,
+    win_return: postbody.win_return,
+    user_name: userName,
   };
   console.log(orderToCreate);
   // check if user exists
@@ -37,7 +39,7 @@ router.post("/", authentication, async function(req, res, next) {
 /**
  * get user's by its status
  */
-router.post("/getOrders", authentication, async function(req, res, next) {
+router.post("/getOrders", authentication, async function (req, res, next) {
   console.log(req);
   const accessToken = req.cookies.access_token;
   const authData = jwt.verify(accessToken, "yolofootball");
@@ -45,7 +47,7 @@ router.post("/getOrders", authentication, async function(req, res, next) {
   const userData = {
     ids: JSON.parse(req.body.order_ids),
     state: req.body.order_state, //pending, completed, canceled
-    created_by: userName
+    created_by: userName,
   };
   console.log(userData);
   // check if user exists
